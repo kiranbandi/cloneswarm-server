b17e878950b4239fe6619b89b15f2e94a14b32ee
kiranbandi <bvenkatkiran@gmail.com>
2018-08-03 15:04:23 -0600

    basic clone detection script functionality complete

--- process-rep.sh
+++ process-rep.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 # Author : Venkat Kiran Bandi
 
+# Set parameters passed to the shell script
+UNIQUE_ID=$1
+REPO_NAME=$2
+PROJECT_NAME=$3
 # Name of the repository is hardcoded temporarily 
 # But will be set from the shell params
 REPO_NAME="https://github.com/alibaba/Sentinel.git"
+PROJECT_NAME="Sentinel"
 
-if [[ -z "$1" ]]
+
+if [[ -z "$UNIQUE_ID" ]]
     then 
         echo "Exiting because , No ID passed for the project"
     else
-        if [[ -z "$2" ]]
+        if [[ -z "$REPO_NAME" ]]
             then
                 echo "No Git repository provided"
             else
-            # Print the name of the unique ID with which the shell script was launched
-            echo "Unqiue ID for Project: $1"
-            # Go to root path  
-            cd ~/workspace 
-            #Create an empty directory for that unique ID 
-            PROJ_DIRECTORY="sandbox-$1"
-            mkdir "$PROJ_DIRECTORY"
-            # Go into that directory 
-            cd "$PROJ_DIRECTORY"
-            # Clone the GIT Repository 
-            git clone "$REPO_NAME"
-            # Go back one step in the working directory
-            cd ..
-            # Run Nicad on the project
-            nicad4 functions java "$PROJ_DIRECTORY" defaultreport
+                # Go to root path  
+                cd ~/workspace 
+                #Create an empty directory for that unique ID 
+                PROJ_DIRECTORY="sandbox-$1"
+                mkdir "$PROJ_DIRECTORY"
+                # Go into that directory 
+                cd "$PROJ_DIRECTORY"
+                # Clone the GIT Repository 
+                git clone "$REPO_NAME"
+                # Run Nicad on the project
+                nicad4 functions java "$PROJECT_NAME" defaultreport
         fi
 fi
 
